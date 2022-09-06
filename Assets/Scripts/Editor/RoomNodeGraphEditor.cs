9d41e25e35bb493e96908ee263d516dbe41a1810
Hayri Mert Kurt <hayrimertkurt@outlook.com>
2022-09-06 20:45:30 +0300

    1.3 Dragging a room node in the editor

--- Assets/Scripts/Editor/RoomNodeGraphEditor.cs
+++ Assets/Scripts/Editor/RoomNodeGraphEditor.cs
@@ -10,6 +10,7 @@ public class RoomNodeGraphEditor : EditorWindow
 {
     private GUIStyle roomNodeStyle;
     private static RoomNodeGraphSO currentRoomNodeGraph;
+    private RoomNodeSO currentRoomNode = null;
     private RoomNodeTypeListSO roomNodeTypeList;
 
     //Node Layout Values
@@ -78,8 +79,34 @@ public class RoomNodeGraphEditor : EditorWindow
 
     private void ProcessEvents(Event currentEvent) 
     {
-        ProcessRoomNodeGraphEvents(currentEvent);
+        if (currentRoomNode==null|| currentRoomNode.isLeftClickDragging==false)
+        {
+            currentRoomNode= IsMouseOverRoomNode(currentEvent);
+        }
+        if (currentRoomNode==null)
+        {
+            ProcessRoomNodeGraphEvents(currentEvent);
+        }
+        else
+        {
+            currentRoomNode.ProcessEvents(currentEvent);
+        }
+        
     }
+
+
+    private RoomNodeSO IsMouseOverRoomNode(Event currentEvent)
+    {
+        for (int i = currentRoomNodeGraph.roomNodeList.Count - 1; i >=0 ; i--)
+        {
+            if (currentRoomNodeGraph.roomNodeList[i].rect.Contains(currentEvent.mousePosition))
+            {
+                return currentRoomNodeGraph.roomNodeList[i];
+            }
+        }
+        return null;
+    }
+
     //Process Room Node Graph Events
     private void ProcessRoomNodeGraphEvents(Event currentEvent)
     {
